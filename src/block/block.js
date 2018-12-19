26bcf3ff8fc413e279ea50440c895e5a22aa9ac8
Crystal Preston <Crystal.Preston@dotmailer.com>
2018-12-19 13:46:16 +0000

    tidyup and created external js file to make window object accessable and separate code

--- src/block/block.js
+++ src/block/block.js
@@ -1,41 +1,20 @@
-/**
- * BLOCK: dd-block
- *
- * Registering a basic block with Gutenberg.
- * Simple block, renders and saves the same content without any interactivity.
- */
 
-//  Import CSS.
 import './style.scss';
 import './editor.scss';
 
-const { __ } = wp.i18n; // Import __() from wp.i18n
-const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+const { __ } = wp.i18n;
+const { registerBlockType } = wp.blocks;
 const el = wp.element.createElement;
 const Components = wp.components;
 var $ = require('jQuery');
-
+var tools = require('./tools.js');
 import { SelectControl } from '@wordpress/components';
 import { withState } from '@wordpress/compose';
 
-/**
- * Register: aa Gutenberg Block.
- *
- * Registers a new block provided a unique name and an object defining its
- * behavior. Once registered, the block is made editor as an option to any
- * editor interface where blocks are implemented.
- *
- * @link https://wordpress.org/gutenberg/handbook/block-api/
- * @param  {string}   name     Block name.
- * @param  {Object}   settings Block settings.
- * @return {?WPBlock}          The block, if it has been successfully
- *                             registered; otherwise `undefined`.
- */
 registerBlockType( 'cgb/block-dd-block', {
-	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __( 'dd-block - Dotdigital Block' ), // Block title.
-	icon: 'shield', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
-	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+	title: __( 'dd-block - Dotdigital Block' ), 
+	icon: 'shield', 
+	category: 'common',
 	keywords: [
 		__( 'dd-block — Dotdigital Block' ),
 		__( 'CGB Example' ),
@@ -46,24 +25,15 @@ registerBlockType( 'cgb/block-dd-block', {
 			type: 'string',
 		}
 	},
-
-	/**
-	 * The edit function describes the structure of your block in the context of the editor.
-	 * This represents what the editor will render when the block is used.
-	 *
-	 * The "edit" property must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 */
     edit: function( props ) {
+		dotsurvey.externalConnectionTest();
         var id = props.attributes.id || '',
 			focus = props.focus;
-        var retval = [];
-
+		var retval = [];
         	const MySelectControl = () => (
         		<SelectControl
 					label={ __( 'Select a survey:' ) }
-					value={ id } // e.g: value = [ 'a', 'c' ]
+					value={ id } 
 					onChange={ (survey) =>props.setAttributes({
 						id: survey
 					})}
@@ -77,9 +47,16 @@ registerBlockType( 'cgb/block-dd-block', {
             retval.push(
                  el( MySelectControl )
 			);
+			retval.push(
+				React.createElement(
+					'div',
+					{ 'id': 'survey-container' }
+				)
+			);
 			if(props.attributes.id) {
-				$('#inspector-select-control-1').closest('.editor-block-list__block-edit').append('<iframe src="'+props.attributes.id+'"></iframe>');
+				$('#survey-container').html('<iframe onload="dotsurvey.resizeIframe(this)" src="'+props.attributes.id+'"></iframe>');
 			}
+			
         return retval;
 	},
 
